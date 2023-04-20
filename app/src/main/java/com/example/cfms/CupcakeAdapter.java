98ee85dbca6942f470bd4c7404327c251d5ff764
Rumesh Pathum <rumeshpathum@gmail.com>
2023-04-20 12:18:59 +0530

    First Commit - Recycler view

--- app/src/main/java/com/example/cfms/CupcakeAdapter.java
+++ app/src/main/java/com/example/cfms/CupcakeAdapter.java
@@ -1,10 +1,14 @@
 package com.example.cfms;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,8 +16,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
 import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,7 +40,7 @@ public class CupcakeAdapter extends FirebaseRecyclerAdapter<CupcakeModel,Cupcake
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull CupcakeModel model) {
+    protected void onBindViewHolder(@NonNull myViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull CupcakeModel model) {
         holder.name.setText(model.getName());
         holder.price.setText(model.getPrice());
 
@@ -66,6 +76,60 @@ public class CupcakeAdapter extends FirebaseRecyclerAdapter<CupcakeModel,Cupcake
                 description.setText(model.getDescription());
 
                 dialogPlus.show();
+
+                //update
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("name",name.getText().toString());
+                        map.put("price",price.getText().toString());
+                        map.put("description",description.getText().toString());
+                        map.put("photo",photoURL.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("cupcakes")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.name.getContext(),"Update Successful",Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(Exception e) {
+                                        Toast.makeText(holder.name.getContext(),"Error, try again",Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+                    }
+                });
+            }
+        });
+
+        //delete
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.name.getContext());
+                builder.setTitle("Are you sure you want to delete this?");
+                builder.setMessage("Deleted data can't be recovered!");
+
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        FirebaseDatabase.getInstance().getReference().child("cupcakes")
+                                .child(getRef(position).getKey()).removeValue();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast.makeText(holder.name.getContext(),"Cancelled",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
